ee46abb1ae40773eced13060b2b6688634f59fba
Vadim <vadimgreen41@gmail.com>
2022-07-07 13:08:02 +0300

    Add Gradle changes

--- app/src/main/java/com/example/homework4_5/domain/model/Comment.kt
+++ app/src/main/java/com/example/homework4_5/domain/model/Comment.kt
@@ -1,7 +1,9 @@
 package com.example.homework4_5
 
 import androidx.annotation.DrawableRes
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Comment(
     @DrawableRes val authorImage: Int,
     val authorName: String,
